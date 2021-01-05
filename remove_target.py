899a50b151e59474f2d595533988543b01e29651
Benni Mack <benni@typo3.org>
2021-01-05 16:45:57 +0100

    Rework python scripts structure
    * Adds utils.py to define all paths
    * Adds add_target.py and remove_target.py
    * A few lines of documentation in the README.md

--- remove_target.py
+++ remove_target.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Load a target file into an existing Repository
+# Removes an existing target file from a repository
 
 # import our own "utils" module
 from utils import *
@@ -13,16 +13,14 @@ basefolder = 'tuf-testrepo'
 keystore = 'tufkeystore'
 reponame = 'tufrepo'
 
-absolute_source = sys.argv[1]
-absolute_source = os.path.abspath(absolute_source)
-target_location = sys.argv[2]
+target_location = sys.argv[1]
 absolute_target = os.path.abspath(os.path.join(basefolder, reponame, 'targets', target_location));
 
 print('Load existing TUF repository')
-repository = tuf.load_repo(basefolder, reponame)
+repository = load_repo(basefolder, reponame)
 
 print('Load signing keys into repo')
-tuf.load_signing_keys_into_repo(repository, keystore)
+load_signing_keys_into_repo(repository, keystore)
 
-print('Adding target ', target_location, ' to repo')
-tuf.add_target(repository, target_location, absolute_source, absolute_target)
+print('Removing target ', target_location, ' from repo')
+remove_target(repository, target_location, absolute_target)
